b4c33c30c03c4542bdb306fcd1c43f2e0b3530b6
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-11-10 08:12:44 -0800

    create_child_process.c

---
+++ create_child_process.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * create_child_process - function
+ * @token: input
+ * Return: void
+ */
+void create_child_process(char *token[])
+{
+	pid_t child;
+	int status;
+
+	child = fork();
+	if (child == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	else if (child == 0)
+	{
+		if (_strcmp(tokens[0], "exit") == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			if (execve(tokens[0], tokens, NULL) == -1)
+			{
+				exit(1);
+			}
+		}
+	}
+			else
+			{
+				waitpid(child, &status, 0);
+			}
+}
